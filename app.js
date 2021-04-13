1b81ee275974ec3ce3030fa69a19775b94649e87
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-13 01:53:36 -0400

    added the https native from node.js to pull data from and API

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
+const https = require('https');
 
 app.get('/', (req, res) => {
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=F7fe8c7b0e6e25b7a8864650f413ad72&units=metric';
+  https.get(url, (response) => {
+    console.log(response);
+    console.log(response.statusCode);
+  });
+
   res.send('Server is up and running for you');
 });
 
